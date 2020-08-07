ba53ff2b9b9ba74bde27195e1b37213a560d592a
Michael Bucher <michael.bucher2@uzh.ch>
2020-08-07 12:44:21 +0200

    Updated deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,12 @@
 ##author: MichaelBucher
 ##date: 25.06.2020
 
+#remove abi folder from submodule idetix to make sure every .sol is compiled again
+cd idetix
+rm -rf abi
+
 #compile and deploy the contracts
-cd idetix/contracts
+cd contracts
 truffle compile
 truffle migrate --reset
 
