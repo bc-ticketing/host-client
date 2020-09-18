68212fb2ae02fe6b48d2c5b5b110fbd17c49a0d1
Michael Bucher <michael.bucher2@uzh.ch>
2020-09-18 10:49:01 +0200

    #7: Added selection of number of of verification methods and showing input fields accordingly

--- src/components/ApproverRegisterForm.vue
+++ src/components/ApproverRegisterForm.vue
@@ -16,8 +16,8 @@
             </div>
           </div>
 
-          <div v-if="showNrLevels" class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100 info-dialog">
               <md-field>
                 <label for="nr-of-levels">Number of Levels</label>
                 <md-select
@@ -29,36 +29,80 @@
                   <md-option value="1">1</md-option>
                   <md-option value="2">2</md-option>
                   <md-option value="3">3</md-option>
+                  <md-option value="4">4</md-option>
+                  <md-option value="5">5</md-option>
                 </md-select>
               </md-field>
+              <div class="info-dialog-button">
+                <md-button
+                  class="md-icon-button md-primary"
+                  @click="showNumberOfLevelsDialog = true"
+                >
+                  <md-icon>help_outline</md-icon>
+                </md-button>
+              </div>
             </div>
           </div>
 
+          <md-dialog :md-active.sync="showNumberOfLevelsDialog">
+            <md-dialog-title>Security Levels</md-dialog-title>
+            <p class="dialog-text">
+              Choose the number of levels that you want to provide for identity verification.
+              The higher the level the more secure should the verification process be.
+              Consider, that for an event that requires e.g. your level 2 verification, anyone
+              with a level 2 or higher verification from you can buy a ticket for this event.
+              So a higher level always comes with permission for your lower specified levels.
+            </p>
+            <p></p>
+            <md-button class="md-primary" @click="showNumberOfLevelsDialog = false">Close</md-button>
+          </md-dialog>
+
           <div class="md-layout-item md-small-size-100">
             <md-field>
-              <label for="firstLevel">First Level</label>
+              <label for="firstLevel">First Level - Lowest Level</label>
               <md-input name="firstLevel" id="firstLevel" v-model="form.firstLevel" />
             </md-field>
           </div>
 
-          <div class="md-layout-item md-small-size-100">
+          <div v-if="form.numberOfLevels >= 2" class="md-layout-item md-small-size-100">
             <md-field>
               <label for="secondLevel">Second Level</label>
               <md-input name="secondLevel" id="secondLevel" v-model="form.secondLevel" />
             </md-field>
           </div>
 
-          <div class="md-layout-item md-small-size-100">
+          <div v-if="form.numberOfLevels >= 3" class="md-layout-item md-small-size-100">
             <md-field>
               <label for="thirdLevel">Third Level</label>
               <md-input name="thirdLevel" id="thirdLevel" v-model="form.thirdLevel" />
             </md-field>
           </div>
 
+          <div v-if="form.numberOfLevels >= 4" class="md-layout-item md-small-size-100">
+            <md-field>
+              <label for="fourthLevel">Fourth Level</label>
+              <md-input name="fourthLevel" id="fourthLevel" v-model="form.fourthLevel" />
+            </md-field>
+          </div>
+
+          <div v-if="form.numberOfLevels >= 5" class="md-layout-item md-small-size-100">
+            <md-field>
+              <label for="fifthLevel">Fifth Level</label>
+              <md-input name="fifthLevel" id="fifthLevel" v-model="form.fifthLevel" />
+            </md-field>
+          </div>
+
           <div class="md-layout-item md-small-size-100">
             <md-field>
-              <label for="approver-url">Approver URL</label>
+              <label for="approver-url">URL</label>
               <md-input name="approver-url" id="approver-url" v-model="form.approverURL" />
+            </md-field>
+          </div>
+
+          <div class="md-layout-item md-small-size-100">
+            <md-field>
+              <label for="twitter">Twitter</label>
+              <md-input name="twitter" id="twitter" v-model="form.approverTwitter" />
             </md-field>
           </div>
         </md-card-content>
@@ -96,18 +140,24 @@ export default {
   name: "ApproverRegisterForm",
   data() {
     return {
+      showNumberOfLevelsDialog: false,
       ipfsHash: "QmYWGJaqiYUPu5JnuUhVVbyXB6g6ydxcie3iwrbC7vxnNP",
       ipfsArgs: null,
       ipfsData: null,
       ipfsString: null,
       form: {
+        // ipfs info
         approverTitle: "Idetix",
-        numberOfLevels: 1,
+        methods: [],
         firstLevel: "email",
         secondLevel: "mobile phone",
         thirdLevel: "kyc",
-        methods: [],
-        approverURL: "http://www.idetix.ch"
+        fourthLevel: "",
+        fifthLevel: "",
+        approverURL: "http://www.idetix.ch",
+        approverTwitter: "",
+        // blockchain info
+        numberOfLevels: 1
       },
       sending: false,
       showNrLevels: false
@@ -143,17 +193,31 @@ export default {
           value: this.form.thirdLevel
         });
       }
+      if (this.form.fourthLevel != "") {
+        methodArray.push({
+          level: 4,
+          value: this.form.fourthLevel
+        });
+      }
+      if (this.form.fifthLevel != "") {
+        methodArray.push({
+          level: 5,
+          value: this.form.fifthLevel
+        });
+      }
       return methodArray;
     }
   },
   methods: {
     createIpfsString() {
+      var methods = this.approverMethods.slice(0, this.form.numberOfLevels);
       let json = JSON.stringify({
         version: "1.0",
         approver: {
           title: this.form.approverTitle,
-          methods: this.approverMethods,
-          url: this.form.approverURL
+          methods: methods,
+          url: this.form.approverURL,
+          twitter: this.form.approverTwitter
         }
       });
       console.log(json);
