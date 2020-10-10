57fe6c5e2960b3eed84554fd37b245e3d69db864
Michael Bucher <michael.bucher2@uzh.ch>
2020-10-10 17:20:41 +0200

    Removed unneeded commented code from store

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,7 @@ import state from "./state";
 import getWeb3 from "../util/getWeb3";
 import getIpfs from "../util/getIpfs";
 import { Event } from "../util/event";
-import { FungibleTicketType, NonFungibleTicketType, NonFungibleTicket } from "../util/tickets";
 
-import { argsToCid } from "idetix-utils";
 import { EVENT_FACTORY_ABI, EVENT_FACTORY_ADDRESS } from "../util/constants/EventFactory";
 import { IDENTITY_ABI, IDENTITY_ADDRESS } from "../util/constants/Identity";
 import { EVENT_MINTABLE_AFTERMARKET_PRESALE_ABI } from "../util/constants/EventMintableAftermarketPresale";
@@ -36,51 +34,14 @@ export default new Vuex.Store({
       console.log("setting identity contract");
       state.identity = identity;
     },
-    // setEventAddresses(state, addresses) {
-    //   console.log("setting event addresses");
-    //   state.eventAddresses = addresses;
-    // },
     updateEventStore(state, events) {
       console.log("setting events");
       state.events = events;
       console.log(events);
       console.log(state.events.length);
     }
-    // addEventMetadata(state, event) {
-    //   console.log("setting event metadata");
-    //   console.log(event);
-    //   console.log(state.events);
-    //   state.events[event.contractAddress].metadata = event.metadata;
-    // }
   },
   actions: {
-    // async loadIpfsEventMetadata({ commit }) {
-    //   console.log("loadIpfsEventMetadata Action being executed");
-    //   for (const contract_address in state.events) {
-    //     console.log("load metadata for: " + contract_address);
-    //     const e = state.events[contract_address];
-    //     try {
-    //       var ipfsData = null;
-    //       for await (const chunk of state.ipfsInstance.cat(e.ipfsHash, {
-    //         timeout: 2000
-    //       })) {
-    //         ipfsData = Buffer(chunk, "utf8").toString();
-    //       }
-    //       var temp = {
-    //         ipfsHash: e.ipfsHash,
-    //         contractAddress: contract_address,
-    //         metadata: JSON.parse(ipfsData)
-    //       };
-    //       console.log(temp);
-    //       commit("addEventMetadata", temp);
-    //     } catch (error) {
-    //       if (error.name == "TimeoutError") {
-    //         console.log("timeout while fetching ipfs metadata");
-    //       }
-    //       console.log(error);
-    //     }
-    //   }
-    // },
     async registerIpfs({ commit }) {
       console.log("registerIpfs Action being executed");
       const ipfsInstance = await getIpfs();
@@ -144,55 +105,5 @@ export default new Vuex.Store({
         await e.loadNonFungibleTickets(state.web3.web3Instance, EVENT_MINTABLE_AFTERMARKET_PRESALE_ABI, state.ipfsInstance);
       }
     }
-    // async loadEvents({ commit }) {
-    //   console.log("loadEvents Action being executed");
-    //   var ipfsHashes = {};
-    //   for (let i = 0; i < state.eventAddresses.length; i++) {
-    //     var addr = state.eventAddresses[i];
-    //     try {
-    //       const eventContract = new state.web3.web3Instance.eth.Contract(
-    //         EVENT_MINTABLE_AFTERMARKET_PRESALE_ABI,
-    //         addr
-    //       );
-    //       const eventMetadata = await eventContract.getPastEvents("EventMetadata", {
-    //         fromBlock: 1
-    //       });
-    //       var metadataObject = eventMetadata[0].returnValues;
-    //       ipfsHashes[addr] = {
-    //         address: addr,
-    //         ipfsHash: argsToCid(
-    //           metadataObject.hashFunction,
-    //           metadataObject.size,
-    //           metadataObject.digest
-    //         )
-    //       }
-    //     } catch {
-    //       console.log("could not get metadata for event");
-    //     }
-    //   }
-    //   commit("setEvents", ipfsHashes);
-    //   // following used before
-    //   // const web3 = await getWeb3();
-    //   // const eventAddresses = await web3.eventFactory.methods
-    //   //   .getEvents()
-    //   //   .call();
-    //   // var i;
-    //   // var events = [];
-    //   // for (i = 0; i < eventAddresses.length; i++) {
-    //   //   var eventInstance = new web3.web3Instance.eth.Contract(
-    //   //     EVENT_MINTABLE_AFTERMARKET_PRESALE_ABI,
-    //   //     eventAddresses[i]
-    //   //   );
-    //   //   console.log(eventInstance);
-    //   //   var owner = await eventInstance.methods.getOwner().call();
-    //   //   // if a non-active address should be used,
-    //   //   //  `web3.account` should be replaced accordingly in the following condition.
-    //   //   if (web3.account == owner) {
-    //   //     events.push(eventInstance);
-    //   //   }
-    //   // }
-    //   // console.log("commiting result to registerEventList mutation");
-    //   // commit("setEvents", events);
-    // }
   }
 });
