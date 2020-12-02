15f05457bb0a8f9d0c1eeffeb672165abfa4cb0f
Michael Bucher <michael.bucher2@uzh.ch>
2020-12-02 15:20:13 +0100

    Removed unneeded code for a local running ipfs node

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueIpfs from "./plugins/vue-ipfs";
 import Vuelidate from "vuelidate"
 
-// Load our IPFS plugin.
-Vue.use(VueIpfs);
 Vue.use(Vuelidate)
 
 Vue.config.productionTip = false;
--- src/util/getIpfs.js
+++ src/util/getIpfs.js
@@ -1,23 +1,5 @@
-import IpfsHttpClient, { CID } from "ipfs-http-client";
 import axios from "axios";
 import { IPFS_TIMEOUT } from "./constants/constants"
-
-const ipfs = new IpfsHttpClient({
-  host: "localhost",
-  port: 5001,
-  protocol: "http"
-});
-
-// const globalIpfs = new HttpClient({
-//   host: "ipfs.io/ipfs/",
-//   protocol: "https"
-// })
-
-function getIpfs() {
-  return ipfs;
-}
-
-export default getIpfs;
 
 export async function getJSONFromIpfs(hash) {
   console.log("hash start");
@@ -35,35 +17,3 @@ export async function getJSONFromIpfs(hash) {
     return false;
   }
 }
-
-//   // console.log(response);
-//   // if (response.status == 200) {
-//   //   return response.data;
-//   // }
-//   // return false;
-
-//   // .then(response => console.log(response)).catch(e => {
-//   //   console.log(e);
-//   //   return false;
-//   // });
-
-
-
-//   // const http = new XMLHttpRequest();
-//   // http.open("GET", url);
-//   // http.send();
-
-//   // http.onreadystatechange=(e)=> {
-//   //   console.log(http.responseText);
-//   // }
-//   var xmlHttp = new XMLHttpRequest();
-//   const url = "http://ipfs.io/ipfs/" + hash;
-//   console.log(url)
-//   xmlHttp.open( "GET", url, false ); // false for synchronous request
-//   xmlHttp.send( null );
-//   if (xmlHttp.status == 200) {
-//     return JSON.parse(xmlHttp.responseText);
-//   }
-//   console.log("error");
-//   return false;
-// }
